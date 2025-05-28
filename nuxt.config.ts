c4574bdf09d342b191b92c283676ea18162c46d4
daeun lee <dix.delee@gmail.com>
2025-05-28 17:44:25 +0900

    add server url to connect webview

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   ssr: true,
+
+  vite: {
+    server: {
+      allowedHosts: ["c1b9-175-193-34-14.ngrok-free.app"],
+    },
+  },
+
+  modules: ["@nuxt/image"],
 });
