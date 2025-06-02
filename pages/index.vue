de57e3a0988c456090c365da6936d34f266a7ce3
daeun lee <dix.delee@gmail.com>
2025-06-02 17:45:47 +0900

    add splash screen

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useRouter } from "nuxt/app";
+
+const router = useRouter();
+
+onMounted(() => {
+  setTimeout(() => {
+    router.push("/home");
+  }, 3000);
+});
+</script>
 
 <template>
   <div class="welcome">
-    <h1>Deepen QT에 오신 걸 환영합니다!</h1>
-    <div class="button"><NuxtLink to="/chat">채팅방으로 이동</NuxtLink></div>
+    <img src="/img/img_symbol.svg" alt="splash" />
+    <h1>Deepen QT</h1>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .welcome {
-  border: 3px solid #ccc;
-  padding: 10px;
+  background-color: #090607;
   margin-bottom: 10px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 95vh;
-  max-width: 600px;
+  width: 375px;
+  height: 812px;
+  // max-width: 600px;
+  // height: 95vh;
   margin: auto;
   padding: 16px;
   box-sizing: border-box;
+
+  h1 {
+    background: linear-gradient(to right, #4e5899, #ce70ca);
+    background-clip: text;
+    color: transparent;
+  }
 
   .button {
     border: 2px solid purple;
