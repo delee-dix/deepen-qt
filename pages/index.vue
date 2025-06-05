64fb82575c5e6da1b7f606a53cffc4ab7ffdc4bb
daeun lee <dix.delee@gmail.com>
2025-06-05 12:03:26 +0900

    fix(style): splash full width

--- pages/index.vue
+++ pages/index.vue
@@ -26,10 +26,11 @@ onMounted(() => {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 375px;
-  height: 812px;
-  // max-width: 600px;
-  // height: 95vh;
+  width: 100%;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  position: fixed;
   margin: auto;
   padding: 16px;
   box-sizing: border-box;
