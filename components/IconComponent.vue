2a978a1bed97cdef5dd865d000e835ae5ca19a41
bwlee-dix <137477179+bwlee-dix@users.noreply.github.com>
2025-06-05 14:39:24 +0900

    feat(CommonComponent): create Image & Icon

---
+++ components/IconComponent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import { computed } from "vue";
+
+  const props = defineProps<{
+    path: string;
+    width?: number;
+    height?: number;
+  }>();
+
+  const iconPath = computed(() => `/icon/${props.path}.svg`);
+</script>
+
+<template>
+  <img :src="iconPath" :alt="path" :width="width" :height="height" />
+</template>
---
+++ components/ImageComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  import { computed } from "vue";
+
+  const props = defineProps<{
+    path: string;
+    width?: number;
+    height?: number;
+    style?: Record<string, string>;
+  }>();
+
+  const iconPath = computed(() => `/img/${props.path}.png`);
+</script>
+
+<template>
+  <img :src="iconPath" :alt="path" :width="width" :height="height" :style="style" draggable="false" />
+</template>
