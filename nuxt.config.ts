d95b060d0df00c75f35aeb1c24c1b0de6f017096
bwlee-dix <137477179+bwlee-dix@users.noreply.github.com>
2025-06-05 14:08:35 +0900

    feat(Foundation): build color foundation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   ssr: true,
-
+  css: ["~/asset/scss/global.scss"],
   vite: {
     server: {
       allowedHosts: ["c1b9-175-193-34-14.ngrok-free.app"],
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/asset/scss/foundation" as *;',
+        },
+      },
     },
   },
 
