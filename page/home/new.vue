b4158a73f5facd2d8c331100952656b178f791b3
bwlee-dix <137477179+bwlee-dix@users.noreply.github.com>
2025-06-09 17:07:41 +0900

    feat(New): separate Bubble component from Chat Page

--- page/home/new.vue
+++ page/home/new.vue
@@ -21,36 +21,22 @@
 
     <div class="chat-container">
       <div class="chat-user-container">
-        <div class="chat-user-bubble">
-          <div class="chat-user-bubble-text">
-            I can't concentrate well these days while doing QT. I'm stressed out because of my children. They're my
-            beloved children...
-          </div>
-        </div>
+        <ChatUserBubble
+          text="I can't concentrate well these days while doing QT. I'm stressed out because of my children. They're my beloved children..."
+        />
       </div>
       <div class="chat-bot-container">
         <IconComponent path="ic_symbol" :width="32" :height="32" />
-        <div class="chat-bot-bubble">
-          <div class="chat-bot-bubble-text">
-            I really understand that feeling. Having a teenager is a special challenge. I think that even the doubts
-            about your qualifications as a parent are actually an expression of your earnest desire to be a good parent.
-          </div>
-        </div>
-        <div class="chat-bot-bubble">
-          <div class="chat-bot-bubble-quote-bar"></div>
-          <div class="chat-bot-bubble-text">
-            "Children, obey your parents in the Lord, for this is right. Honor your father and mother," which is the
-            first commandment with promise, so that it may be well with you and you may live long on the earth. And,
-            fathers, do not provoke your children to anger, but bring them up in the training and instruction of the
-            Lord." (Ephesians 6:1-4)
-          </div>
-        </div>
-        <div class="chat-bot-bubble">
-          <div class="chat-bot-bubble-text">
-            Okay. Today's 매일성경 is covering a scene from 1 Samuel 17. Have you ever heard the story of David and
-            Goliath?
-          </div>
-        </div>
+        <ChatBotBubble
+          text="I really understand that feeling. Having a teenager is a special challenge. I think that even the doubts about your qualifications as a parent are actually an expression of your earnest desire to be a good parent."
+        />
+        <ChatBotBubble
+          text="'Children, obey your parents in the Lord, for this is right. Honor your father and mother, which is the first commandment with promise, so that it may be well with you and you may live long on the earth. And, fathers, do not provoke your children to anger, but bring them up in the training and instruction of the Lord. (Ephesians 6:1-4)'"
+          isQuote
+        />
+        <ChatBotBubble
+          text="Okay. Today's 매일성경 is covering a scene from 1 Samuel 17. Have you ever heard the story of David and Goliath?"
+        />
         <div class="feed-back-container">
           <IconComponent path="ic_copy" :width="24" :height="24" />
           <IconComponent path="ic_thumbs_up" :width="24" :height="24" />
@@ -58,12 +44,9 @@
         </div>
       </div>
       <div class="chat-user-container">
-        <div class="chat-user-bubble">
-          <div class="chat-user-bubble-text">
-            I can't concentrate well these days while doing QT. I'm stressed out because of my children. They're my
-            beloved children...
-          </div>
-        </div>
+        <ChatUserBubble
+          text="I can't concentrate well these days while doing QT. I'm stressed out because of my children. They're my beloved children..."
+        />
       </div>
     </div>
     <InputChat v-model="chatContent" />
@@ -97,27 +80,8 @@
         height: fit-content;
         padding: 20px 0;
         box-sizing: border-box;
-
-        .chat-user-bubble {
-          display: flex;
-          flex-direction: column;
-          width: fit-content;
-          height: fit-content;
-          padding: 24px 16px;
-          border-radius: 8px;
-          background: $gradient-bubble;
-
-          .chat-user-bubble-text {
-            font-size: 16px;
-            font-weight: 400;
-            line-height: 150%;
-            text-align: left;
-            letter-spacing: -0.02em;
-            color: $body-active;
-            width: fit-content;
-          }
-        }
       }
+
       .chat-bot-container {
         display: flex;
         flex-direction: column;
@@ -126,31 +90,6 @@
         padding: 20px 0;
         gap: 16px;
         box-sizing: border-box;
-
-        .chat-bot-bubble {
-          display: flex;
-          flex-direction: row;
-          width: fit-content;
-          height: fit-content;
-          border-radius: 8px;
-          gap: 8px;
-
-          .chat-bot-bubble-text {
-            font-size: 16px;
-            font-weight: 400;
-            line-height: 150%;
-            text-align: left;
-            letter-spacing: -0.02em;
-            color: $body-active;
-            width: fit-content;
-          }
-
-          .chat-bot-bubble-quote-bar {
-            width: 4px;
-            height: 100%;
-            background: $white;
-          }
-        }
 
         .feed-back-container {
           display: flex;
