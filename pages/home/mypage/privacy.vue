b099c4b6dd6bd09ad7442a0d34dd1dca759b89d9
daeun lee <dix.delee@gmail.com>
2025-06-09 11:47:36 +0900

    create privacy policy screen

--- pages/home/mypage/privacy.vue
+++ pages/home/mypage/privacy.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="mypage-container">
+  <div class="privacy-container">
     <div class="empty-area"></div>
     <div class="content-area">
       <!-- <div class="bar"></div> -->
@@ -14,12 +14,36 @@
         <div>Privacy Policy</div>
         <div></div>
       </div>
+      <div class="privacy-content">
+        <div class="intro">
+          Deepen QT (“we,” “our,” or “us”) is committed to protecting your
+          privacy. This Privacy Policy describes how we collect, use, and share
+          information when you use our app and services. By using Deepen QT, you
+          agree to the terms of this Privacy Policy.
+        </div>
+        <div class="one">
+          <strong>1. Information We Collect</strong><br />
+          We may collect the following types of information:<br /><br />
+          Personal information (such as your name, email address)<br /><br />
+          Usage data (including log information, device type, and interaction
+          data)<br /><br />
+          Content you share within the app (prayer notes, reflections, etc.)
+        </div>
+        <div class="two">
+          <strong>2. How We Use Your Information</strong><br />
+          We use the information we collect to:<br /><br />
+          Provide and maintain the app<br /><br />
+          Personalize your experience (e.g., devotional suggestions)<br /><br />
+          Improve app performance and features<br /><br />
+          Communicate with you (e.g., updates, announcements)
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.mypage-container {
+.privacy-container {
   background-color: #090607;
   color: #c6c6c6;
   display: flex;
@@ -58,6 +82,16 @@
       justify-content: space-between;
       align-items: center;
       margin-top: 32px;
+    }
+
+    .privacy-content {
+      display: flex;
+      flex-direction: column;
+      gap: 14px;
+
+      .intro {
+        margin-top: 24px;
+      }
     }
   }
 }
