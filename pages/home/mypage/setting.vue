a4e210422eb2123766f0739b918a9810df6f0f16
daeun lee <dix.delee@gmail.com>
2025-06-09 11:47:44 +0900

    create setting screen

--- pages/home/mypage/setting.vue
+++ pages/home/mypage/setting.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const value = ref(50);
+const isSelected = ref<boolean>(false);
+</script>
 
 <template>
-  <div class="mypage-container">
+  <div class="setting-container">
     <div class="empty-area"></div>
     <div class="content-area">
       <!-- <div class="bar"></div> -->
@@ -14,12 +19,50 @@
         <div>Setting</div>
         <div>Save</div>
       </div>
+      <div class="setting-content">
+        <div class="title">Font & Theme</div>
+        <div class="font-theme">
+          <div class="font-size">
+            <div class="sub-title">Font Size</div>
+            <div class="slider-container">
+              <div :style="{ fontSize: '17px' }">aA</div>
+              <input
+                type="range"
+                id="volume"
+                min="0"
+                max="100"
+                step="1"
+                v-model="value"
+                class="slider"
+              />
+              <div :style="{ fontSize: '24px' }">aA</div>
+            </div>
+          </div>
+          <div class="font">
+            <div class="sub-title">Font</div>
+            <div class="buttons">
+              <div class="button selected">Helvetica</div>
+              <div class="button">Arial</div>
+            </div>
+          </div>
+          <div class="theme">
+            <div class="sub-title">Theme</div>
+            <div class="buttons">
+              <div class="button selected">System</div>
+              <div class="button">Dark</div>
+              <div class="button">Light</div>
+            </div>
+          </div>
+        </div>
+        <div class="title">Alarm</div>
+        <div class="alarm-picker" :style="{ height: '178px' }">(picker)</div>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.mypage-container {
+.setting-container {
   background-color: #090607;
   color: #c6c6c6;
   display: flex;
@@ -58,6 +101,69 @@
       justify-content: space-between;
       align-items: center;
       margin-top: 32px;
+    }
+
+    .setting-content {
+      display: flex;
+      flex-direction: column;
+      gap: 16px;
+
+      .font-theme,
+      .alarm-picker {
+        display: flex;
+        flex-direction: column;
+        background-color: #363636;
+        border-radius: 2px;
+        padding: 16px;
+
+        .slider-container {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          gap: 16px;
+          margin: 8px auto;
+          text-align: center;
+
+          .slider {
+            width: 80%;
+            height: 4px;
+            border-radius: 50%;
+            background-color: #d987ee;
+          }
+        }
+      }
+
+      .title {
+        font-size: 16px;
+        margin-top: 25px;
+      }
+
+      .sub-title {
+        font-size: 14px;
+        gap: 16px;
+      }
+
+      .buttons {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        width: 311px;
+        gap: 8px;
+        margin: 8px auto;
+
+        .button {
+          border-radius: 4px;
+          background-color: #161616;
+          padding: 12px 16px;
+          font-size: 12px;
+          color: #8e8e8e;
+
+          &.selected {
+            background: linear-gradient(to right, #4e5899, #ce70ca);
+            color: #ffffff;
+          }
+        }
+      }
     }
   }
 }
