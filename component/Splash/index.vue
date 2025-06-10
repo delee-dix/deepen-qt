f0da367027871cf59927f11d2e6a7667deb3ca0f
bwlee-dix <137477179+bwlee-dix@users.noreply.github.com>
2025-06-11 08:50:25 +0900

    fix(Splash): adjust style

--- component/Splash/index.vue
+++ component/Splash/index.vue
@@ -1,49 +1,47 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
-import { useRouter } from "nuxt/app";
+  import { onMounted } from "vue";
+  import { useRouter } from "nuxt/app";
 
-const router = useRouter();
+  const router = useRouter();
 
-onMounted(() => {
-  setTimeout(() => {
-    router.push("/");
-  }, 3000);
-});
+  onMounted(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  });
 </script>
 
 <template>
-  <div class="welcome">
-    <img src="/icon/ic_symbol.svg" alt="splash" />
-    <h1>Deepen QT</h1>
+  <div class="splash-container">
+    <CommonImage src="/icon/ic_symbol.svg" alt="splash" :height="204" />
+    <div class="title">Deepen QT</div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.welcome {
-  background-color: #090607;
-  margin-bottom: 10px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
-  top: 0;
-  left: 0;
-  position: fixed;
-  margin: auto;
-  padding: 16px;
-  box-sizing: border-box;
+  .splash-container {
+    background-color: #090607;
+    margin-bottom: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    position: fixed;
+    margin: auto;
+    padding: 16px;
+    box-sizing: border-box;
+    gap: 20px;
 
-  h1 {
-    background: linear-gradient(to right, #4e5899, #ce70ca);
-    background-clip: text;
-    color: transparent;
+    .title {
+      font-size: 40px;
+      font-weight: 700;
+      background: linear-gradient(to right, #4e5899, #ce70ca);
+      background-clip: text;
+      color: transparent;
+    }
   }
-
-  .button {
-    border: 2px solid purple;
-    padding: 10px;
-  }
-}
 </style>
